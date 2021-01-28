bebc981bbace98e7fca98e0ee3fadac9410bf56e
Cyrus Tanade <cyrus.tanade@duke.edu>
2021-01-28 16:19:51 -0500

    added analysis function and added HDL_driver to interface

--- blood_tests.py
+++ blood_tests.py
@@ -6,19 +6,24 @@ def interface():
         choice = input("Enter an option: ")
         if choice == "9":
             return
+        if choice == "1":
+            HDL_driver()
 
 def getData():
     print("Enter HDL level: ")
     userInput = input()
     return userInput
 
+def analyzeData(data):
+    if int(data) >= 60:
+        print("Normal")
+    elif (int(data) >= 40) & (int(data) < 60):
+        print("Borderline Low")
+    else:
+        print("Low")
 
 def HDL_driver():
-    # Get Data
     data = getData()
-
-    # Analyze Data
-
-    # Output Data
+    analyzeData(data)
 
 interface()
