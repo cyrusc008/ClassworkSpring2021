40889646e752b679568ea19a34141bdf6c8fa5a8
Cyrus Tanade <cyrus.tanade@duke.edu>
2021-01-28 15:42:37 -0500

    multiple returns

--- return.py
+++ return.py
@@ -1,6 +1,11 @@
 def my_function(x):
-  a = x+5
-  return a
+  a = x + 5
+  if a < 0:
+    answer = "negative number"
+  else:
+    answer = "positive number"
+  return a, answer
 
-y = my_function(-20)
+y, z = my_function(-20)
 print(y)
+print(z)
