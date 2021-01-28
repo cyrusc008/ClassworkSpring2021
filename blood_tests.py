94010fa10df12c9a8bea2cb24b44533b0146f815
Cyrus Tanade <cyrus.tanade@duke.edu>
2021-01-28 16:24:11 -0500

    added some features to interface

--- blood_tests.py
+++ blood_tests.py
@@ -2,11 +2,12 @@ def interface():
     print("Blood Test Analysis")
     while True:
         print("\nOptions")
+        print("1 - HDL")
         print("9 - Quit")
         choice = input("Enter an option: ")
         if choice == "9":
             return
-        if choice == "1":
+        elif choice == "1":
             HDL_driver()
 
 def getData():
