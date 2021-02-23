c9d3b1fa89fdcce7fc14787dd0090bbb4db05461
Cyrus Tanade <cyrus.tanade@duke.edu>
2021-02-23 16:42:32 -0500

    data logging

---
+++ logger.py
@@ -0,0 +1,23 @@
+import logging
+
+
+def fibonacci(fib_list):
+    next_number = fib_list[-1] + fib_list[-2]
+    logging.warning("{} + {} = {}".format(fib_list[-1], fib_list[-2], next_number))
+    fib_list.append(next_number)
+    if next_number < 100:
+        fibonacci(fib_list)
+    else:
+        logging.info("Reached end of list")
+    return fib_list
+
+
+def main():
+    x = [0, 1]
+    answer = fibonacci(x)
+    print(answer)
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="fig.log", level=logging.INFO,
+                        filemode="w")
+    main()
