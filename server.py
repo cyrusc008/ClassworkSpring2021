0624b3c1ed353c635e4ff9c7dd81cd7f07283ccf
Cyrus Tanade <cyrus.tanade@duke.edu>
2021-03-16 16:40:49 -0400

    server assignment

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 
 app = Flask(__name__)
@@ -6,7 +6,36 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def server_stats():
-    return "The server is on."
+    return "The server is on again."
+
+
+@app.route("/info", methods=["GET"])
+def information():
+    return "This server will allow the user to request blood analyses."
+
+
+@app.route("/HDL", methods=["POST"])
+def HDL_request():
+    """
+        input info: {"HDL": 60}
+    """
+    from blood_tests import analyzeHDL
+    in_data = request.get_json()
+    print(in_data)
+    HDL = in_data["HDL"]
+    result = analyzeHDL(HDL)
+    answer = {"HDL": HDL, "Analysis": result}
+    if answer != "normal":
+        return "Bad HDL", 400
+    return jsonify(answer)
+
+
+@app.route("/say_hello/<name>/<age>", methods=["GET"])
+def say_hello_function(name, age):
+    next_year = int(age) + 1
+    output = "Hello there, {}! You will be {} old next year".format(
+        name, next_year)
+    return output
 
 
 if __name__ == "__main__":
